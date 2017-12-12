3bc344a2078345342f526c3d4293cb10fc092f6b
Aleksey Voskresenskiy <aleksey.voskresenskiy@gmail.com>
2017-12-12 10:10:46 +0300

    copyrights

--- src/components/Main/Admin/Interface/Constructor/RadioButtonColor.tsx
+++ src/components/Main/Admin/Interface/Constructor/RadioButtonColor.tsx
@@ -1,3 +1,19 @@
+// Copyright 2017 The apla-front Authors
+// This file is part of the apla-front library.
+//
+// The apla-front library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The apla-front library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the apla-front library. If not, see <http://www.gnu.org/licenses/>.
+
 import * as React from 'react';
 import * as classnames from 'classnames';
 import styled from 'styled-components';
