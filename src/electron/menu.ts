f773cd865d277a31e4390fa3d6c31d710283149f
Saurer <redmmond@gmail.com>
2018-01-04 02:47:32 +0900

    Added developer tools to electron app

--- src/electron/menu.ts
+++ src/electron/menu.ts
@@ -15,23 +15,41 @@
 // along with the apla-front library. If not, see <http://www.gnu.org/licenses/>.
 
 import { MenuItemConstructorOptions, app, Menu } from 'electron';
+import { window } from './windows';
 
-const template: MenuItemConstructorOptions[] = [{
-    label: 'Apla',
-    submenu: [
-        { label: 'Quit', accelerator: 'Command+Q', click: app.quit }
-    ]
-}, {
-    label: 'Edit',
-    submenu: [
-        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
-        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
-        { type: 'separator' },
-        { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
-        { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
-        { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
-        { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectall' }
-    ]
-}];
+const template: MenuItemConstructorOptions[] = [
+    {
+        label: 'Apla',
+        submenu: [
+            { label: 'Quit', accelerator: 'Command+Q', click: app.quit }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
+            { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
+            { type: 'separator' },
+            { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
+            { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
+            { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
+            { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectall' }
+        ]
+    },
+    {
+        label: 'Help',
+        submenu: [
+            {
+                label: 'Toggle Developer Tools', click: () => {
+                    if (window) {
+                        window.webContents.openDevTools({
+                            mode: 'detach'
+                        });
+                    }
+                }
+            }
+        ]
+    }
+];
 
 export default Menu.buildFromTemplate(template);
