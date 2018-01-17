7efc2a4317887e783e7bdfe4e9fc0cb3cd81af21
Saurer <redmmond@gmail.com>
2018-01-18 04:27:16 +0900

    Style optimization

--- src/app/components/Protypo/handlers/StyledComponent.tsx
+++ src/app/components/Protypo/handlers/StyledComponent.tsx
@@ -23,6 +23,9 @@ interface IStyledComponentProps {
     style?: string;
 }
 
-export default function styledComponent<T>(Component: TComponentConstructor<T & IStyledComponentProps>) {
-    return styled(Component) `${props => props.style}`;
+export default function styledComponent<T extends {}>(Component: TComponentConstructor<T & IStyledComponentProps>) {
+    const StyledComponent = styled(Component) `${props => props.style}`;
+    return (props: T & IStyledComponentProps) => props.style ?
+        <StyledComponent {...props} /> :
+        <Component {...props} />;
 }
