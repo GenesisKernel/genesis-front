c63fe9a54f278fd084255d1b1a1de472ff0c04b2
Saurer <redmmond@gmail.com>
2018-04-21 21:38:40 +0900

    Split app to container and component

--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -15,85 +15,53 @@
 // along with the genesis-front library. If not, see <http://www.gnu.org/licenses/>.
 
 import * as React from 'react';
-import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { IRootState } from 'modules';
 import { IntlProvider } from 'react-intl';
-import platform from 'lib/platform';
-import * as classnames from 'classnames';
-
-import { AnimatedSwitch } from 'components/Animation';
-import Main from 'containers/Main';
-import Auth from 'containers/Auth';
-import Splash from 'components/Splash';
-
-import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
-import InitHook from 'containers/App/InitHook';
 import { switchWindow } from 'modules/gui/actions';
-import ModalProvider from 'containers/Modal/ModalProvider';
+import HTML5Backend from 'react-dnd-html5-backend';
 
-interface IAppProps {
+import App from 'components/App';
+
+export interface IAppContainerProps {
+}
+
+interface IAppContainerState {
     locale: string;
     isAuthenticated: boolean;
     isLoaded: boolean;
     isCollapsed: boolean;
-    isOffline: boolean;
     localeMessages: { [key: string]: string };
-    switchWindow?: typeof switchWindow.started;
 }
 
-class App extends React.Component<IAppProps> {
-    componentWillReceiveProps(props: IAppProps) {
-        if (this.props.isAuthenticated !== props.isAuthenticated) {
-            props.switchWindow(props.isAuthenticated ? 'main' : 'general');
-        }
-    }
-
-    render() {
-        const classes = classnames({
-            'wrapper': true,
-            'layout-fixed': true,
-            'aside-collapsed': this.props.isCollapsed,
-            'aside-toggled': !this.props.isCollapsed,
-            'platform-desktop': platform.select({ desktop: true }),
-            'platform-web': platform.select({ web: true }),
-        });
-
-        return (
-            <IntlProvider locale={this.props.locale} defaultLocale="en-US" messages={this.props.localeMessages}>
-                <div className={classes}>
-                    <InitHook />
-                    <ModalProvider />
-                    <AnimatedSwitch animation={AnimatedSwitch.animations.fade()}>
-                        {!this.props.isLoaded && (
-                            <Route path="/" component={Splash} />
-                        )}
-                        {!this.props.isAuthenticated && (
-                            <Route path="/" component={Auth} />
-                        )}
-                        <Route path="/" component={Main} />
-                    </AnimatedSwitch>
-                </div>
-            </IntlProvider>
-        );
-    }
+interface IAppContainerDispatch {
+    switchWindow: typeof switchWindow.started;
 }
+
+const AppContainer: React.SFC<IAppContainerProps & IAppContainerState & IAppContainerDispatch> = props => (
+    <IntlProvider locale={props.locale} defaultLocale="en-US" messages={props.localeMessages}>
+        <App
+            isAuthenticated={props.isAuthenticated}
+            isLoaded={props.isLoaded}
+            isCollapsed={props.isCollapsed}
+            switchWindow={props.switchWindow}
+        />
+    </IntlProvider>
+);
 
 const mapStateToProps = (state: IRootState) => ({
     locale: state.storage.locale,
     localeMessages: state.engine.localeMessages,
     isAuthenticated: state.auth.isAuthenticated,
     isCollapsed: state.engine.isCollapsed,
-    isLoaded: state.engine.isLoaded,
-    isOffline: state.engine.isOffline
+    isLoaded: state.engine.isLoaded
 });
 
 const mapDispatchToProps = {
     switchWindow: switchWindow.started
 };
 
-// export default connect(mapStateToProps, mapDispatchToProps, null, { pure: false })(App);
 export default DragDropContext(HTML5Backend)(
-    connect(mapStateToProps, mapDispatchToProps, null, { pure: false })(App)
+    connect<IAppContainerState, IAppContainerDispatch, IAppContainerProps>(mapStateToProps, mapDispatchToProps, null, { pure: false })(AppContainer)
 );
