d42dffa2006dd743c818bd53867e6a7d97f2df02
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-02 18:10:59 +0000

    Deploy GenesisKernel/genesis-front to github.com/GenesisKernel/genesis-front.git:gh-pages

--- src/electron/config.ts
+++
@@ -1,6 +0,0 @@
-import ElectronStore from 'electron-store';
-const Store: typeof ElectronStore = require('electron-store');
-
-export default new Store({
-    cwd: process.platform === 'win32' ? process.cwd() : null
-});
