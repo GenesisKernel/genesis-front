ac1869504fa217c0848e7ff84eafcb5c0225e58a
Frank Horrigan <redmmond@gmail.com>
2018-11-22 16:48:54 +1000

    Implemented config parser

---
+++ src/app/services/config/index.test.ts
@@ -0,0 +1,140 @@
+// MIT License
+// 
+// Copyright (c) 2016-2018 GenesisKernel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Config from './';
+
+interface ISettingsMock {
+    stringParam: string;
+    stringArrayParam: string[];
+    booleanParam: boolean;
+}
+
+const createConfigMock = () =>
+    new Config<ISettingsMock>({
+        stringParam: {
+            type: 'string',
+            defaultValue: ''
+        },
+        stringArrayParam: {
+            type: 'string[]',
+            defaultValue: []
+        },
+        booleanParam: {
+            type: 'boolean',
+            defaultValue: false
+        }
+    });
+
+test('String value', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringParam', 'test')).toMatch('test');
+});
+
+test('String invalid value fallback to proposal', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringParam', 123, 'PROPOSAL')).toMatch('PROPOSAL');
+});
+
+test('String invalid value fallback to multiple proposals', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringParam', 123, 12345678, 'PROPOSAL')).toMatch('PROPOSAL');
+});
+
+test('String invalid value fallback to default', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringParam', 123)).toMatch('');
+});
+
+test('String array value', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringArrayParam', ['test'])).toEqual(['test']);
+});
+
+test('String array invalid value fallback to proposal', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringArrayParam', 'test', ['PROPOSAL'])).toEqual(['PROPOSAL']);
+});
+
+test('String array invalid value fallback to multiple proposals', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringArrayParam', 123, 12345678, ['PROPOSAL'])).toEqual(['PROPOSAL']);
+});
+
+test('String array invalid value fallback to default', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('stringArrayParam', 'test')).toEqual([]);
+});
+
+test('Boolean value', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('booleanParam', true)).toEqual(true);
+});
+
+test('Boolean invalid value fallback to proposal', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('booleanParam', 123, true)).toEqual(true);
+});
+
+test('Boolean invalid value fallback to multiple proposals', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('booleanParam', 123, 12345678, true)).toEqual(true);
+});
+
+test('Boolean invalid value fallback to default', () => {
+    const config = createConfigMock();
+    expect(config.tryGetValue('booleanParam', 123)).toEqual(false);
+});
+
+test('Complex deserialization with truthy values', () => {
+    const config = createConfigMock();
+    expect(config.deserialize({
+        stringParam: 'hello',
+        stringArrayParam: ['world'],
+        booleanParam: true
+    })).toEqual({
+        stringParam: 'hello',
+        stringArrayParam: ['world'],
+        booleanParam: true
+    });
+});
+
+test('Complex deserialization with falsy values', () => {
+    const config = createConfigMock();
+    expect(config.deserialize({
+        stringParam: 123,
+        stringArrayParam: 456,
+        booleanParam: 789
+    })).toEqual({
+        stringParam: '',
+        stringArrayParam: [],
+        booleanParam: false
+    });
+});
+
+test('Complex deserialization with missing values', () => {
+    const config = createConfigMock();
+    expect(config.deserialize({})).toEqual({
+        stringParam: '',
+        stringArrayParam: [],
+        booleanParam: false
+    });
+});
---
+++ src/app/services/config/index.ts
@@ -0,0 +1,77 @@
+// MIT License
+// 
+// Copyright (c) 2016-2018 GenesisKernel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import stringType from './types/string';
+import stringArrayType from './types/stringArray';
+import booleanType from './types/boolean';
+
+export type TSchemaValueType =
+    'string' | 'string[]' |
+    'boolean';
+
+export interface ISchemaValue<T> {
+    type: TSchemaValueType;
+    defaultValue: T;
+}
+
+export type TConfigurationSchema<T> = {
+    [K in keyof T]: ISchemaValue<T[K]>;
+};
+
+export type TConfigValueType<T> = {
+    name: TSchemaValueType;
+    isDefined: (value: any) => boolean;
+    tryGetValue: (value: any, defaultValue: T, ...fallbackValues: any[]) => T;
+};
+
+const configValueTypes: { [K in TSchemaValueType]: TConfigValueType<any> } = {
+    'string': stringType,
+    'string[]': stringArrayType,
+    'boolean': booleanType,
+};
+
+class Config<T> {
+    private _schema: TConfigurationSchema<T>;
+
+    constructor(schema: TConfigurationSchema<T>) {
+        this._schema = schema;
+    }
+
+    public tryGetValue = <K extends keyof T>(key: K, value: any, ...fallbackValues: any[]) => {
+        const valueSchema = this._schema[key];
+        return configValueTypes[valueSchema.type].tryGetValue(value, valueSchema.defaultValue, ...fallbackValues) as T[K];
+    }
+
+    public deserialize = (candidate: any) => {
+        const result: { [K in keyof T]?: T[K] } = {};
+
+        for (let key in this._schema) {
+            if (this._schema.hasOwnProperty(key)) {
+                result[key] = this.tryGetValue(key, candidate[key]);
+            }
+        }
+
+        return result as { [K in keyof T]: T[K] };
+    }
+}
+
+export default Config;
