efaa2111fda82757414c808ee31d21c880d7c4ac
Frank Horrigan <redmmond@gmail.com>
2019-05-18 03:09:45 +1000

    Implemented not connected screen

--- src/app/components/Auth/Offline/Error.tsx
+++ src/app/components/Auth/Offline/Error.tsx
@@ -21,38 +21,26 @@
 // SOFTWARE.
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import NetworkError from 'services/network/errors';
 
-import Error from './Error';
-
-export interface IOfflineProps {
+export interface IErrorProps {
     error: NetworkError;
 }
 
-const Offline: React.SFC<IOfflineProps> = props => (
-    <div>
-        {props.error && (
-            <Error error={props.error} />
-        )}
-        {!props.error && (
-            <div style={{ padding: 30 }}>
-                <i className="fa fa-chain-broken text-primary" style={{ fontSize: 128 }} />
-                <h3>
-                    <FormattedMessage id="general.network.notconnected" defaultMessage="Not connected" />
-                </h3>
-                <div className="text-muted">
-                    <FormattedMessage id="general.network.notconnected.desc" defaultMessage="Please connect to a network to begin using Apla. You can manage networks by clicking on the connection indicator in the top right corner of this window" />
-                </div>
-                <div style={{ marginTop: 25 }}>
-                    <Link to="/networks" className="btn btn-primary">
-                        <FormattedMessage id="general.network.connect" defaultMessage="Connect" />
-                    </Link>
-                </div>
-            </div>
-        )}
+const Error: React.SFC<IErrorProps> = props => (
+    <div className="text-center mv-lg">
+        <h1 className="mb-lg">
+            <sup>
+                <em className="fa fa-cog fa-2x text-muted fa-spin text-info" />
+            </sup>
+            <em className="fa fa-cog fa-5x text-muted fa-spin text-purple" />
+            <em className="fa fa-cog fa-lg text-muted fa-spin text-success" />
+        </h1>
+        <div className="text-bold text-lg mb-lg">
+            <FormattedMessage id={`general.network.error.${props.error}`} defaultMessage="Network is unreachable" />
+        </div>
     </div>
 );
 
-export default Offline;
+export default Error;
