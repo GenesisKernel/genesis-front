ed80863f072033821dc7b2d99fe226c1b5adb688
Frank Horrigan <redmmond@gmail.com>
2019-05-18 05:17:05 +1000

    Force request key info when restoring session

--- src/app/modules/auth/epics/reloadWalletsEpic.ts
+++ src/app/modules/auth/epics/reloadWalletsEpic.ts
@@ -22,9 +22,13 @@
 
 import { Epic } from 'modules';
 import { logout, loadWallets } from '../actions';
-import { discoverNetwork } from 'modules/engine/actions';
+import { discoverNetwork, initialize } from 'modules/engine/actions';
 
-const reloadWalletsEpic: Epic = (action$) => action$.ofType(logout.done.type, discoverNetwork.done.type)
-    .map(action => loadWallets.started(null));
+const reloadWalletsEpic: Epic = (action$, store) => action$.ofType(logout.done.type, discoverNetwork.done.type, initialize.done.type)
+    .filter(l => {
+        const session = store.getState().engine.guestSession;
+        return !!(session && store.getState().storage.networks.find(n => n.uuid === session.network.uuid));
+
+    }).map(action => loadWallets.started(null));
 
 export default reloadWalletsEpic;
